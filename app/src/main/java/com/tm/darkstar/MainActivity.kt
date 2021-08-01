f09d75034ecfb8137caa9f179c214e6b308ffd44
Travis E  Mikolay <TravisEMikolay+GitKraken@Gmail.com>
2021-08-01 12:15:02 -0400

    My First Commit

---
+++ app/src/main/java/com/tm/darkstar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tm.darkstar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
